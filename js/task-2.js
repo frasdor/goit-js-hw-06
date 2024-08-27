cf0cdf2c6add83e84775741724795263e20e6761
frasdor <frasunkiewicz.dorota@gmail.com>
2024-08-27 10:51:13 +0200

    added task 2 and 3

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,21 @@
 class Storage {
-    getItems(),
-    AddItem(),
-    removeItem();
+    constructor(items){
+        this.items = items;
+    }
+    getItems(){
+        return this.items
+    }
+    addItem(newItem){
+        return this.items.push(newItem)
+    }
+    removeItem(itemToRemove){
+        const index = this.items.indexOf(itemToRemove);           
+        if (index !== -1) {                                
+            this.items.splice(index,1);    
+        }
+
+    }
+    
 };
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
@@ -21,3 +35,12 @@ console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
 // getItems() — zwraca tablicę bieżących towarów w prywatnej właściwości items.
 // addItem(newItem) — przyjmuje nowy towar newItem i dodaje go do tablicy towarów w prywatnej właściwości items obiektu.
 // removeItem(itemToRemove) — przyjmuje ciąg znaków z nazwą towaru itemToRemove i usuwa go z tablicy towarów w prywatnej właściwości items obiektu.
+
+
+// // removeItem(itemToRemove){
+//     const index = this.items.indexOf(itemToRemove);           // sprawdzamy czy w tablicy this.items znajduje sie  itemToRemove, jesli jast to indexOf daje jego indeks, jesli nie to wynik jest -1.
+//     if (index !== -1) {                                // sprawdzamy czy index nie jest rowny -1 (czyli,ze itemToremove znajduje sie w tablicy this.items)
+//         this.items.splice(index,1);    // jesli warunek z if zostal spelniony to z tablicy this.items za pomoca metody splice zostanie usuniety ten przedmiot, ktorego index zosatl wyznaczony, a po przecinku oznacz ile ma byc usunietych tych itemow, u nas 1)
+//     }
+
+// }
